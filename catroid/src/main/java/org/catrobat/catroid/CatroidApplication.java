bcecd62d9f6d07f4bcfff546ed0e163bb9305b50
Nur <nureldinelrez@gmail.com>
2017-05-31 13:45:36 +0200

    CAT-2345 Multilingual Feature on Application Level- new

--- catroid/src/main/java/org/catrobat/catroid/CatroidApplication.java
+++ catroid/src/main/java/org/catrobat/catroid/CatroidApplication.java
@@ -23,11 +23,16 @@
 package org.catrobat.catroid;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 import com.parrot.freeflight.settings.ApplicationSettings;
+
+import org.catrobat.catroid.ui.Multilingual;
+
+import java.util.Locale;
 
 public class CatroidApplication extends MultiDexApplication {
 
@@ -39,6 +44,8 @@ public class CatroidApplication extends MultiDexApplication {
 	public static final String OS_ARCH = System.getProperty("os.arch");
 
 	public static boolean parrotLibrariesLoaded = false;
+	public static String defaultSystemLanguage;
+	public static SharedPreferences languageSharedPreferences;
 
 	@Override
 	public void onCreate() {
@@ -46,6 +53,162 @@ public class CatroidApplication extends MultiDexApplication {
 		Log.d(TAG, "CatroidApplication onCreate");
 		settings = new ApplicationSettings(this);
 		CatroidApplication.context = getApplicationContext();
+		defaultSystemLanguage = Locale.getDefault().getLanguage();
+		// open the App in the last chosen language
+		languageSharedPreferences = getSharedPreferences("For_language", getApplicationContext().MODE_PRIVATE);
+		String langTag = languageSharedPreferences.getString("Nur", "");
+		switch (langTag) {
+			case "":
+				Multilingual.setContextLocale(getApplicationContext(), defaultSystemLanguage);
+				break;
+			case "ar":
+				Multilingual.setContextLocale(getApplicationContext(), "ar");
+				break;
+			case "az":
+				Multilingual.setContextLocale(getApplicationContext(), "az");
+				break;
+			case "bs":
+				Multilingual.setContextLocale(getApplicationContext(), "bs");
+				break;
+			case "ca":
+				Multilingual.setContextLocale(getApplicationContext(), "ca");
+				break;
+			case "cs":
+				Multilingual.setContextLocale(getApplicationContext(), "cs");
+				break;
+			case "da":
+				Multilingual.setContextLocale(getApplicationContext(), "da");
+				break;
+			case "de":
+				Multilingual.setContextLocale(getApplicationContext(), "de");
+				break;
+			case "en-rAU":
+				Multilingual.setContextLocale(getApplicationContext(), "en-rAU");
+				break;
+			case "en-rCA":
+				Multilingual.setContextLocale(getApplicationContext(), "en-rCA");
+				break;
+			case "en-rGB":
+				Multilingual.setContextLocale(getApplicationContext(), "en-rGB");
+				break;
+			case "en":
+				Multilingual.setContextLocale(getApplicationContext(), "en");
+				break;
+			case "es":
+				Multilingual.setContextLocale(getApplicationContext(), "es");
+				break;
+			case "fa":
+				Multilingual.setContextLocale(getApplicationContext(), "fa");
+				break;
+			case "fr":
+				Multilingual.setContextLocale(getApplicationContext(), "fr");
+				break;
+			case "gl":
+				Multilingual.setContextLocale(getApplicationContext(), "gl");
+				break;
+			case "gu":
+				Multilingual.setContextLocale(getApplicationContext(), "gu");
+				break;
+			case "iw":
+				Multilingual.setContextLocale(getApplicationContext(), "iw");
+				break;
+			case "hi":
+				Multilingual.setContextLocale(getApplicationContext(), "hi");
+				break;
+			case "hr":
+				Multilingual.setContextLocale(getApplicationContext(), "hr");
+				break;
+			case "hu":
+				Multilingual.setContextLocale(getApplicationContext(), "hu");
+				break;
+			case "in":
+				Multilingual.setContextLocale(getApplicationContext(), "in");
+				break;
+			case "it":
+				Multilingual.setContextLocale(getApplicationContext(), "it");
+				break;
+			case "ja":
+				Multilingual.setContextLocale(getApplicationContext(), "ja");
+				break;
+			case "ko":
+				Multilingual.setContextLocale(getApplicationContext(), "ko");
+				break;
+			case "mk":
+				Multilingual.setContextLocale(getApplicationContext(), "mk");
+				break;
+			case "ml":
+				Multilingual.setContextLocale(getApplicationContext(), "ml");
+				break;
+			case "ms":
+				Multilingual.setContextLocale(getApplicationContext(), "ms");
+				break;
+			case "nl":
+				Multilingual.setContextLocale(getApplicationContext(), "nl");
+				break;
+			case "no":
+				Multilingual.setContextLocale(getApplicationContext(), "no");
+				break;
+			case "pl":
+				Multilingual.setContextLocale(getApplicationContext(), "pl");
+				break;
+			case "ps":
+				Multilingual.setContextLocale(getApplicationContext(), "ps");
+				break;
+			case "pt":
+				Multilingual.setContextLocale(getApplicationContext(), "pt");
+				break;
+			case "pt-rBR":
+				Multilingual.setContextLocale(getApplicationContext(), "pt-rBR");
+				break;
+			case "ro":
+				Multilingual.setContextLocale(getApplicationContext(), "ro");
+				break;
+			case "ru":
+				Multilingual.setContextLocale(getApplicationContext(), "ru");
+				break;
+			case "sd":
+				Multilingual.setContextLocale(getApplicationContext(), "sd");
+				break;
+			case "sl":
+				Multilingual.setContextLocale(getApplicationContext(), "sl");
+				break;
+			case "sq":
+				Multilingual.setContextLocale(getApplicationContext(), "sq");
+				break;
+			case "sr-rCS":
+				Multilingual.setContextLocale(getApplicationContext(), "sr-rCS");
+				break;
+			case "sr-rSP":
+				Multilingual.setContextLocale(getApplicationContext(), "sr-rSP");
+				break;
+			case "sv":
+				Multilingual.setContextLocale(getApplicationContext(), "sv");
+				break;
+			case "ta":
+				Multilingual.setContextLocale(getApplicationContext(), "ta");
+				break;
+			case "te":
+				Multilingual.setContextLocale(getApplicationContext(), "te");
+				break;
+			case "th":
+				Multilingual.setContextLocale(getApplicationContext(), "th");
+				break;
+			case "tr":
+				Multilingual.setContextLocale(getApplicationContext(), "tr");
+				break;
+			case "ur":
+				Multilingual.setContextLocale(getApplicationContext(), "ur");
+				break;
+			case "vi":
+				Multilingual.setContextLocale(getApplicationContext(), "vi");
+				break;
+			case "zh-rCN":
+				Multilingual.setContextLocale(getApplicationContext(), "zh-rCN");
+				break;
+			case "zh-rTW":
+				Multilingual.setContextLocale(getApplicationContext(), "zh-rTW");
+				break;
+		}
 	}
 
 	@Override
--- catroid/src/main/java/org/catrobat/catroid/ui/SettingsActivity.java
+++ catroid/src/main/java/org/catrobat/catroid/ui/SettingsActivity.java
@@ -24,6 +24,7 @@ package org.catrobat.catroid.ui;
 
 import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -98,6 +99,15 @@ public class SettingsActivity extends PreferenceActivity {
 		setHintPreferences();
 		updateActionBar();
 
+		Preference language = findPreference("setting_multilingual");
+		language.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent intent = new Intent(getApplicationContext(), Multilingual.class);
+				startActivity(intent);
+				return false;
+			}
+		});
 		screen = getPreferenceScreen();
 
 		if (!BuildConfig.FEATURE_LEGO_NXT_ENABLED) {
